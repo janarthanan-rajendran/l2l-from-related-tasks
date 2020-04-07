51dd18dc8274e7fe01ae830af973c4d3dd5c450d
Janarthanan Rajendran <jana@jana.local>
2020-04-07 12:46:28 -0400

    code for creating data with different splits

---
+++ MemN2N/split_data.py
@@ -0,0 +1,29 @@
+in_data_dir = './../data/personalized-dialog-dataset/split-by-profile/male_young/'
+out_data_dir = './../data/personalized-dialog-dataset/split-by-profile-75/male_young/'
+file_name = 'personalized-dialog-task5-full-dialogs-trn.txt'
+
+train_data_percent = 75
+dialog_count = 0
+dialog_max_count = train_data_percent/100 * 1000
+
+with open(in_data_dir+file_name,'r') as f_in:
+    with open(out_data_dir+file_name,'w') as f_out:
+        in_lines = f_in.readlines()
+        for line in in_lines:
+            f_out.write(line)
+            line = line.strip()
+            if not line:
+                dialog_count +=1
+                if dialog_count == dialog_max_count:
+                    break
+
+# checking
+dialog_count = 0
+with open(out_data_dir+file_name,'r') as f_out:
+    out_lines = f_out.readlines()
+    for line in out_lines:
+        line = line.strip()
+        if not line:
+            dialog_count +=1
+
+print(dialog_count)
