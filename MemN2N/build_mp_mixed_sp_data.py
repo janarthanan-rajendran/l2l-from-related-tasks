f3b0f69a79cda0b01af4afe031fbcd6e874cfe44
Janarthanan Rajendran <jana@jana.local>
2020-04-27 17:31:52 -0400

    code to create mp mixed data

--- MemN2N/build_mp_mixed_sp_data.py
+++ MemN2N/build_mp_mixed_sp_data.py
@@ -58,32 +58,13 @@ with open(in_data_dir+file_name,'r') as f_in:
                 skip_line = False
             num += 1
 
+# checking
+dialog_count = 0
+with open(out_data_dir+file_name,'r') as f_out:
+    out_lines = f_out.readlines()
+    for line in out_lines:
+        line = line.strip()
+        if not line:
+            dialog_count +=1
 
-
-
-
-#         in_lines = f_in.readlines()
-#         skip_line = True
-#         for line in in_lines:
-#             if not skip_line:
-#                 if line.strip():
-#                     nid, line = line.split(' ',1)
-#                     nid = int(nid)
-#                     f_out.write(str(nid-1) + ' ' + line)
-#                 else:
-#                     f_out.write(line)
-#             skip_line = False
-#             line = line.strip()
-#             if not line:
-#                 skip_line = True
-#
-# # checking
-# dialog_count = 0
-# with open(out_data_dir+file_name,'r') as f_out:
-#     out_lines = f_out.readlines()
-#     for line in out_lines:
-#         line = line.strip()
-#         if not line:
-#             dialog_count +=1
-
-# print(dialog_count)
+print(dialog_count)
